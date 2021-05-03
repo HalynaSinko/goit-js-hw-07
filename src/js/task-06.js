1d4ffbb139e1dcbdccf180c0a7b6d1c842b3001e
Halyna Sinko <galinasinko14@gmail.com>
2021-05-03 20:35:08 +0300

    Изменяет название функций.

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,8 +1,8 @@
 const inputEl = document.querySelector('#validation-input');
 
-inputEl.addEventListener('blur', onInputElValidation);
+inputEl.addEventListener('blur', onInputValidation);
 
-function onInputElValidation() {
+function onInputValidation() {
   if (inputEl.value.length === Number(inputEl.dataset.length)) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
